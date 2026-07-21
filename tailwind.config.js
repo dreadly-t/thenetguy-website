4b255e3a625ecc2d036664034c336d72c241ec3d
Dreadly <ttarn.mountains@gmail.com>
2026-07-21 15:47:28 +1200

    added wellington and manawatu pages - updated our mission section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	content: ['./index.html', './faq/index.html'],
+	content: ['./index.html', './faq/index.html', './wellington/index.html', './manawatu-whanganui/index.html', './WIP pages/**/*.html'],
 	darkMode: 'class',
 	theme: {
 		extend: {
